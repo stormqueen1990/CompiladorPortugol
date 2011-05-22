306e6c3202840ed97e3460fa734002ccc5e69f57
Mauren <stormqueen1990@gmail.com>
2011-05-22 15:57:37 -0300

    Solução de conflito.

--- src/sintatico/ErroSintaxeEnum.java
+++ src/sintatico/ErroSintaxeEnum.java
@@ -17,24 +17,24 @@ public enum ErroSintaxeEnum {
 
 	private Integer codMsg;
 	private String textoMsg;
-
+	
 	private ErroSintaxeEnum(Integer codMsg, String textoMsg) {
 		this.codMsg = codMsg;
 		this.textoMsg = textoMsg;
 	}
-
+	
 	public Integer getCodMsg() {
 		return this.codMsg;
 	}
-
+	
 	public void setCodMsg(Integer codMsg) {
 		this.codMsg = codMsg;
 	}
-
+	
 	public String getTextoMsg() {
 		return this.textoMsg;
 	}
-
+	
 	public void setTextoMsg(String textoMsg) {
 		this.textoMsg = textoMsg;
 	}
