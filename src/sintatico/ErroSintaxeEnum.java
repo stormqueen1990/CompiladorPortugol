e26e0f87388488748c6f52f99ff9792decbb2067
Mauren <stormqueen1990@gmail.com>
2011-05-22 16:01:55 -0300

    Modificações.

---
+++ src/sintatico/ErroSintaxeEnum.java
@@ -0,0 +1,39 @@
+package sintatico;
+
+/**
+ * Enumeração com os tipos de erro sintático possíveis.
+ * 
+ * @author mauren
+ */
+public enum ErroSintaxeEnum {
+	PALAVRA_ALGORITMO_NAO_ENCONTRADA(1, "Palavra-chave \'Algoritmo\' não encontrada."),
+	PALAVRA_INICIO_NAO_ENCONTRADA(2, "Palavra-chave \'Inicio\' não encontrada."),
+	PALAVRA_FIMALGORITMO_NAO_ENCONTRADA(3, "Palavra-chave \'Fimalgoritmo\' não encontrada."),
+	PALAVRA_REPITA_NAO_ENCONTRADA(4, "Palavra-chave \'Repita\' não encontrada."),
+	PALAVRA_ATE_NAO_ENCONTRADA(5, "Palavra-chave \'Ate\' não encontrada.");
+	
+	
+	private Integer codMsg;
+	private String textoMsg;
+	
+	private ErroSintaxeEnum(Integer codMsg, String textoMsg) {
+		this.codMsg = codMsg;
+		this.textoMsg = textoMsg;
+	}
+	
+	public Integer getCodMsg() {
+		return this.codMsg;
+	}
+	
+	public void setCodMsg(Integer codMsg) {
+		this.codMsg = codMsg;
+	}
+	
+	public String getTextoMsg() {
+		return this.textoMsg;
+	}
+	
+	public void setTextoMsg(String textoMsg) {
+		this.textoMsg = textoMsg;
+	}
+}
