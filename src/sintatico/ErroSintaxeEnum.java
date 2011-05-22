ad1ee22329c256db5d84d54977866665ab71b279
shaman <shaman@shaman-ubuntu>
2011-05-22 15:32:40 -0300

    alterações

--- src/sintatico/ErroSintaxeEnum.java
+++ src/sintatico/ErroSintaxeEnum.java
@@ -8,7 +8,9 @@ package sintatico;
 public enum ErroSintaxeEnum {
 	PALAVRA_ALGORITMO_NAO_ENCONTRADA(1, "Palavra-chave \'Algoritmo\' não encontrada."),
 	PALAVRA_INICIO_NAO_ENCONTRADA(2, "Palavra-chave \'Inicio\' não encontrada."),
-	PALAVRA_FIMALGORITMO_NAO_ENCONTRADA(3, "Palavra-chave \'Fimalgoritmo\' não encontrada.");
+	PALAVRA_FIMALGORITMO_NAO_ENCONTRADA(3, "Palavra-chave \'Fimalgoritmo\' não encontrada."),
+	PALAVRA_REPITA_NAO_ENCONTRADA(4, "Palavra-chave \'Repita\' não encontrada."),
+	PALAVRA_ATE_NAO_ENCONTRADA(5, "Palavra-chave \'Ate\' não encontrada.");
 	
 	
 	private Integer codMsg;
