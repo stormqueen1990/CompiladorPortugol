c75f4277fbcb0d8dc854e43ef7bae717b9f543af
Mauren <stormqueen1990@gmail.com>
2011-05-22 16:13:04 -0300

    Adição de novos erros.

--- src/sintatico/ErroSintaxeEnum.java
+++ src/sintatico/ErroSintaxeEnum.java
@@ -10,29 +10,31 @@ public enum ErroSintaxeEnum {
 	PALAVRA_INICIO_NAO_ENCONTRADA(2, "Palavra-chave \'Inicio\' não encontrada."),
 	PALAVRA_FIMALGORITMO_NAO_ENCONTRADA(3, "Palavra-chave \'Fimalgoritmo\' não encontrada."),
 	PALAVRA_REPITA_NAO_ENCONTRADA(4, "Palavra-chave \'Repita\' não encontrada."),
-	PALAVRA_ATE_NAO_ENCONTRADA(5, "Palavra-chave \'Ate\' não encontrada.");
-	
-	
+	PALAVRA_ATE_NAO_ENCONTRADA(5, "Palavra-chave \'Ate\' não encontrada."),
+	PALAVRA_PARA_NAO_ENCONTRADA(6, "Palavra-chave \'Para\' não encontrada."),
+	VARIAVEL_CONTROLE_NAO_ENCONTRADA(7, "Variável de controle do laço não encontrada."),
+	VARIAVEL_CONTROLE_NAO_DECLARADA(8, "Variável de controle do laço não declarada.");
+
 	private Integer codMsg;
 	private String textoMsg;
-	
+
 	private ErroSintaxeEnum(Integer codMsg, String textoMsg) {
 		this.codMsg = codMsg;
 		this.textoMsg = textoMsg;
 	}
-	
+
 	public Integer getCodMsg() {
 		return this.codMsg;
 	}
-	
+
 	public void setCodMsg(Integer codMsg) {
 		this.codMsg = codMsg;
 	}
-	
+
 	public String getTextoMsg() {
 		return this.textoMsg;
 	}
-	
+
 	public void setTextoMsg(String textoMsg) {
 		this.textoMsg = textoMsg;
 	}
